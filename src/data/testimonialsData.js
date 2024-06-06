378e24370157ecde09426fc326be425c8a9f6b73
Thomas Reese <thomas18reese@gmail.com>
2024-06-06 11:52:35 -0500

    added better mock data

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -3,23 +3,23 @@ import image2 from "../assets/t-image2.jpg";
 import image3 from "../assets/t-image3.jpg";
 
 export const testimonialsData = [
-  {
-    image: image1,
-    review:
-      "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-    name: 'MATHEW HENDRICKSON',
-    status : 'ENTREPRENEUR'
-  },
-  {
-    image: image2,
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
-    name: 'JOHN KEVIN',
-    status: 'COACH'
-  },
-  {
-    image : image3,
-    review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
-    name: 'FRANKLIN',
-    status: "CUSTOMER"
-  }
-];
+    {
+      image: image1,
+      review:
+        "I made the right choice by choosing the FitClub and with the right plan and program, I already achieved my ideal body!",
+      name: 'MATHEW HENDRICKSON',
+      status: 'ENTREPRENEUR'
+    },
+    {
+      image: image2,
+      review: 'The trainers at FitClub are highly knowledgeable and motivating. I have seen amazing results in just a few months!',
+      name: 'JOHN KEVIN',
+      status: 'COACH'
+    },
+    {
+      image: image3,
+      review: 'FitClub has transformed my life. The community is supportive and the facilities are top-notch. I look forward to my workouts every day.',
+      name: 'FRANKLIN WILLIAMS',
+      status: 'CUSTOMER'
+    }
+  ];
