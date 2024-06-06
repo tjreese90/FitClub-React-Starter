0705cab2bfbd634eb068064a550adbeff9251407
Thomas Reese <thomas18reese@gmail.com>
2024-06-06 12:46:02 -0500

    finsihed the footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-import './Footer.css'
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+import React from "react";
+import "./Footer.css";
+import GH from "../../assets/github.png";
+import IG from "../../assets/instagram.png";
+import LI from "../../assets/linkedin.png";
+import logo from "../../assets/logo.png";
+import "../../App.css"
 
-export default Footer
+const Footer = () => {
+	return (
+		<div className="Footer-Container">
+			<hr />
+			<div className="footer">
+				<div className="social-links">
+					<img src={GH} alt="" />
+					<img src={IG} alt="" />
+					<img src={LI} alt="" />
+				</div>
+
+				<div className="logo-f">
+					<img src={logo} alt="" />
+				</div>
+			</div>
+            <div className="blur footer-blur-1"></div>
+            <div className="blur footer-blur-2"></div>
+		</div>
+	);
+};
+
+export default Footer;
